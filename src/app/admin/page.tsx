50c74496f66d345c1ea699466062a98d1766768c
Ahmed <roouamazon55@gmail.com>
2025-07-02 15:08:22 +0000

    Runtime Error
    Error: ./src/app/admin/page.tsx:104:10
    Parsing ecmascrip

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -96,7 +96,7 @@ export default function AdminPage() {
                 variant: 'destructive',
             });
             setIsUploading(false);
-        }
+        };
         reader.readAsText(selectedFile);
     };
 
