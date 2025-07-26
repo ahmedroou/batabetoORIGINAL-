a3a0afb8b73e77d1ec563ab919b2b7a439110a4e
Ahmed <roouamazon55@gmail.com>
2025-07-26 11:26:42 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ✅ هذا هو الخيار المطلوب
-
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -18,7 +16,7 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
+   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       "handlebars": "handlebars/dist/handlebars.js",
