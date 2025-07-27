9b6ad142dffe6560a4042a810fba7d489201e3b3
Ahmed <roouamazon55@gmail.com>
2025-07-27 14:54:03 +0000

    npm error code 1 npm error path /workspace npm error command failed npm

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    // This is to allow the Next.js dev server to be proxied in the editor.
-    allowedDevOrigins: [
-      '9000-firebase-studio-1751446606209.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
-    ],
-  },
   images: {
     remotePatterns: [
       {
@@ -21,13 +15,6 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      handlebars: 'handlebars/dist/handlebars.js',
-    };
-    return config;
   },
 };
 
