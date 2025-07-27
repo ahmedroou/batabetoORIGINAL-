e9caf3fefc51016ca2e9d827e6d6a22de3303e88
Ahmed <roouamazon55@gmail.com>
2025-07-27 14:50:36 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    // This is to allow the Next.js dev server to be proxied in the editor.
+    allowedDevOrigins: [
+      '9000-firebase-studio-1751446606209.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
+    ],
+  },
   images: {
     remotePatterns: [
       {
@@ -16,10 +22,10 @@ const nextConfig = {
       },
     ],
   },
-   webpack: (config) => {
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "handlebars": "handlebars/dist/handlebars.js",
+      handlebars: 'handlebars/dist/handlebars.js',
     };
     return config;
   },
