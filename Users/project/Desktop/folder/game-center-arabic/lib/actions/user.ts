b2983b6ae8c3c623914272cfd82cc822bd24b1d2
Ahmed <roouamazon55@gmail.com>
2025-08-08 03:18:27 +0000

    قم باعادة اختبارات النصوص لكن تاكد من انك تنفذ تعليماتها بشكل جيد وتحسن

---
+++ Users/project/Desktop/folder/game-center-arabic/lib/actions/user.ts
@@ -0,0 +1,71 @@
+
+
+/**
+ * @fileoverview This file re-exports all user-related actions from their new, modular locations.
+ * This structure improves maintainability by separating concerns.
+ */
+
+// Explicitly import and export to avoid namespace collisions and help bundlers.
+import { createUserProfile, updateUserName, updateUserAvatar, updateUserGender } from './user/profile';
+import { purchaseAvatar, purchasePunishmentAvatar, exchangeCoinsForHonor } from './user/currency';
+import { getSocialRankForUser, getPlayerFromUserId, getGameKings, getKingOfGames, getAllUsers, updateUserWinCount, searchUsers } from './user/queries';
+import { sendSystemMail, getMail, markMailAsRead, claimMailCoins } from './user/mail';
+import { 
+    getLeagueData, 
+    updateUserStats, 
+    createLeague, 
+    joinLeague, 
+    deleteLeague, 
+    kickPlayerFromLeague, 
+    leaveLeague, 
+    resetAllLeagueStats, 
+    updateLeagueScoresForGameEnd,
+    calculateEndOfGameAwards 
+} from './user/leagues';
+import { giveReward, applyPunishment, humiliatePlayer, requestAllegiance, issueDecree, begForMercy, demandTaxes, respondToTaxDemand, requestAlliance, respondToAlliance, issueDuelChallenge, respondToDuelChallenge, forceAvatarChange, payPunishmentTax, exchangeForLoyaltyPoints } from './user/social';
+
+export {
+    createUserProfile,
+    updateUserName,
+    updateUserAvatar,
+    updateUserGender,
+    purchaseAvatar,
+    purchasePunishmentAvatar,
+    exchangeCoinsForHonor,
+    getSocialRankForUser,
+    getPlayerFromUserId,
+    getGameKings,
+    getKingOfGames,
+    getAllUsers,
+    updateUserWinCount,
+    searchUsers,
+    sendSystemMail,
+    getMail,
+    markMailAsRead,
+    claimMailCoins,
+    getLeagueData,
+    updateUserStats,
+    createLeague,
+    joinLeague,
+    deleteLeague,
+    kickPlayerFromLeague,
+    leaveLeague,
+    resetAllLeagueStats,
+    updateLeagueScoresForGameEnd,
+    calculateEndOfGameAwards,
+    giveReward,
+    applyPunishment,
+    humiliatePlayer,
+    requestAllegiance,
+    issueDecree,
+    begForMercy,
+    demandTaxes,
+    respondToTaxDemand,
+    requestAlliance,
+    respondToAlliance,
+    issueDuelChallenge,
+    respondToDuelChallenge,
+    forceAvatarChange,
+    payPunishmentTax,
+    exchangeForLoyaltyPoints
+};
