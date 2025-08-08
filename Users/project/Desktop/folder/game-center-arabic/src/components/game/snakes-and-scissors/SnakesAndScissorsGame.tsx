e3c294c4677e7631c8c9c0812e4341eb0d6ae547
Ahmed <roouamazon55@gmail.com>
2025-08-08 18:22:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- Users/project/Desktop/folder/game-center-arabic/src/components/game/snakes-and-scissors/SnakesAndScissorsGame.tsx
+++ Users/project/Desktop/folder/game-center-arabic/src/components/game/snakes-and-scissors/SnakesAndScissorsGame.tsx
@@ -3,10 +3,9 @@
 
 import type { Game, Player } from '@/types';
 import { AnimatePresence, motion } from 'framer-motion';
-import { FinalResultsPhase } from '@/components/game/snakes-and-scissors/phases/FinalResultsPhase';
-import { LobbyPhase } from '@/components/game/snakes-and-scissors/phases/LobbyPhase';
+import { FinalResultsPhase } from '@/components/game/prison/phases/FinalResultsPhase';
+import { LobbyPhase } from '@/components/game/prison/phases/LobbyPhase';
 import { MonopolyGame } from '@/components/game/monopoly/MonopolyGame';
-
 
 interface SnakesAndScissorsGameProps {
     game: Game;
@@ -14,7 +13,6 @@ interface SnakesAndScissorsGameProps {
 }
 
 export function SnakesAndScissorsGame({ game, self }: SnakesAndScissorsGameProps) {
-
     const renderContent = () => {
         switch (game.gameState) {
             case 'lobby':
@@ -33,7 +31,7 @@ export function SnakesAndScissorsGame({ game, self }: SnakesAndScissorsGameProps
         }
     };
 
-     return (
+    return (
         <AnimatePresence mode="wait">
             <motion.div
                 key={game.gameState}
