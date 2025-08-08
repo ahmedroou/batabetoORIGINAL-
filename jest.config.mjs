df5a291b87100900a6cd1e7109e542dbe7067b86
Ahmed <roouamazon55@gmail.com>
2025-08-08 03:27:08 +0000

    ● Validation Error:
      Module ts-jest in the transform option was not fo

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,48 +1,22 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+// jest.config.mjs
+import nextJest from 'next/jest.js'
 
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
-const config = {
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-
-  // The test environment that will be used for testing
-  testEnvironment: "jsdom",
-
-  // A map from regular expressions to paths to transformers
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
   transformIgnorePatterns: [
-    '/node_modules/(?!yaml).+\\.js$'
+      "/node_modules/(?!yaml/)",
   ],
-  
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-};
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
 
-export default config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(customJestConfig)
