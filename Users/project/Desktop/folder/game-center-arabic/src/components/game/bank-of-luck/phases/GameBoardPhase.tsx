728283b7c0e8368cd7003a4d56544d0c221f858f
Ahmed <roouamazon55@gmail.com>
2025-08-09 11:08:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ Users/project/Desktop/folder/game-center-arabic/src/components/game/bank-of-luck/phases/GameBoardPhase.tsx
@@ -0,0 +1,26 @@
+
+'use client';
+
+import type { Game, Player } from '@/types';
+import { GameBoard } from '../GameBoard';
+import { ActionPanel } from '../ActionPanel';
+
+interface GameBoardPhaseProps {
+    game: Game;
+    self: Player;
+}
+
+export function GameBoardPhase({ game, self }: GameBoardPhaseProps) {
+    const isMyTurn = game.bankOfLuckState?.turnOrder[game.bankOfLuckState.currentTurnIndex] === self.id;
+
+    return (
+        <div className="w-full h-screen p-4 grid grid-cols-1 md:grid-cols-3 gap-4 bg-gray-100 dark:bg-gray-900">
+            <div className="md:col-span-2 flex items-center justify-center">
+                <GameBoard game={game} self={self} />
+            </div>
+            <div className="md:col-span-1">
+                <ActionPanel game={game} self={self} isMyTurn={isMyTurn} />
+            </div>
+        </div>
+    );
+}
