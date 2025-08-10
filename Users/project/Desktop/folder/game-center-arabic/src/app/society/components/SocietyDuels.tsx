a5eb25ccb889460edabe5e99562b23a596d353cf
Ahmed <roouamazon55@gmail.com>
2025-08-10 15:07:12 +0000

    لماذا لم تقم بازالة الملفات عدية الفائدة من هيكل الكود

--- Users/project/Desktop/folder/game-center-arabic/src/app/society/components/SocietyDuels.tsx
+++ Users/project/Desktop/folder/game-center-arabic/src/app/society/components/SocietyDuels.tsx
@@ -1,5 +1,2 @@
 // This file is no longer used and can be safely deleted.
 // The "Duels" tab has been replaced by the "Prison" tab in `app/society/client.tsx`.
-export default function SocietyDuels() {
-    return null;
-}
