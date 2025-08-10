ee0a032b670c6cbead0c8986c9f6d3259a9b98dc
Ahmed <roouamazon55@gmail.com>
2025-08-10 14:00:25 +0000

    فين الاصلاحات ؟؟

--- Users/project/Desktop/folder/game-center-arabic/src/app/admin/components/QuestionManagementTab.tsx
+++ Users/project/Desktop/folder/game-center-arabic/src/app/admin/components/QuestionManagementTab.tsx
@@ -24,7 +24,7 @@ import {
   uploadWordWarWordsFromJson,
   deleteDuplicateWords,
   uploadPrisonQuestionsFromJson,
-  getPublicTrapAnswerCategories,
+  getTrapAnswerCategories,
 } from '@/lib/actions/admin';
 import { Game } from '@/types';
 import { getDrawAndGuessCategories, addDrawAndGuessCategory, editDrawAndGuessCategory, deleteDrawAndGuessCategory, uploadDrawAndGuessPromptsFromJson } from '@/lib/actions/draw-and-guess-admin';
@@ -72,7 +72,7 @@ export default function QuestionManagementTab() {
     const fetchCategories = async (gameType: 'trap-answer' | 'draw-and-guess') => {
         let categoriesResult;
         if (gameType === 'trap-answer') {
-            categoriesResult = await getPublicTrapAnswerCategories();
+            categoriesResult = await getTrapAnswerCategories();
             if (categoriesResult.success && categoriesResult.categories) {
                 setTrapAnswerCategories(categoriesResult.categories.sort((a,b) => a.localeCompare(b)));
             }
