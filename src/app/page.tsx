dce1e76d8dcd70237b7f15135b21d211a9c63b6f
Ahmed <roouamazon55@gmail.com>
2025-08-12 23:29:25 +0000

    ارجع التحديات النشطة في الصفحة الرئيسية لتظهر التحديات بشكل صغير للغاية

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ import HomeDialogs from "./components/home/Dialogs";
 import WelcomeGuest from "./components/home/WelcomeGuest";
 import MainLoadingSkeleton from "./components/home/MainLoadingSkeleton";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import SocietyChallenges from "./society/components/SocietyChallenges";
+import { CompactChallengeList } from "./components/home/CompactChallengeList";
 
 export default function Home() {
     const { user, userProfile, loading, socialRanks, getSocialRankForUser, activeChallenges, newChallengeAvailable, markChallengeAsSeen } = useAuth();
@@ -81,7 +81,7 @@ export default function Home() {
                                 <CardDescription>انضم إلى التحديات الحالية واربح جوائز قيمة!</CardDescription>
                             </CardHeader>
                             <CardContent>
-                               <SocietyChallenges filter="active" />
+                               <CompactChallengeList challenges={activeChallenges} />
                             </CardContent>
                         </Card>
                     )}
