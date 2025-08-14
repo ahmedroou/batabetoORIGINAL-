5e06765d50d066d62cc95ad1fc1beed0e4e6fe5e
Ahmed <roouamazon55@gmail.com>
2025-08-14 07:05:10 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -24,9 +24,6 @@ const nextConfig = {
     }
     return config;
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@opentelemetry/instrumentation'],
-  },
 };
 
 module.exports = nextConfig;
