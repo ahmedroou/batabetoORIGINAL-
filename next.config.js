85edc416336cde395be88ce41d4f2ceb2730f7e0
Ahmed <roouamazon55@gmail.com>
2025-08-14 06:00:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -16,6 +16,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
