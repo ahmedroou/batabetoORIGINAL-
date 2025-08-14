05628fa48e244bb6ea17d2d541d4b1ea4b63c2fd
Ahmed <roouamazon55@gmail.com>
2025-08-14 17:11:51 +0000

    مازال التطبيق لا يتصل بقاعدة البيانات ابدا

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,4 @@
 
-      
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -9,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAFUCN6QzQ3BiLS8KCkFvwkWo9gY0kvTt4",
   authDomain: "deep-dive-the-friendship-game.firebaseapp.com",
   projectId: "deep-dive-the-friendship-game",
-  storageBucket: "deep-dive-the-friendship-game.firebasestorage.app",
+  storageBucket: "deep-dive-the-friendship-game.appspot.com",
   messagingSenderId: "359342640267",
   appId: "1:359342640267:web:24bcfbb409bc8bbb83d88e",
   measurementId: "G-86EHFM7J3L"
@@ -22,5 +21,3 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
-
-    
