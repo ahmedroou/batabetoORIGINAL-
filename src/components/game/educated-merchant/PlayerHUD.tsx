1410b851f2cc99b23d0e174e212e87e876f7d9b7
Ahmed <roouamazon55@gmail.com>
2025-08-27 13:17:41 +0000

    تمام انحلت هذه المشكلة لكن الشريط الجانبي مازال يضيء كل شوي هل هناك حل ؟

--- src/components/game/educated-merchant/PlayerHUD.tsx
+++ src/components/game/educated-merchant/PlayerHUD.tsx
@@ -52,7 +52,6 @@ const PlayerRow = React.memo(function PlayerRow({
 
   return (
     <motion.div
-      layout
       initial={{ opacity: 0, y: 6 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -6 }}
